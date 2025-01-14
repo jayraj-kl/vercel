c0e5cf5ed878946aee304dbd2840edc19937715d
Jayraj Ladkat <jayrajkladkat@gmail.com>
2025-01-15 02:18:19 +0530

    init

---
+++ upload-service/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from "cors";
+
+const app= express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors())
+app.use(express.json());
+
+app.post('/deploy', (req, res) => {
+    const repoURL = req.body.repoURL;
+    res.send(`Deploying ${repoURL}...`);
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
